6268e8f19b45fc1a162db17c810adbb9723a06cf
ericksantos2 <116085135+ericksantos2@users.noreply.github.com>
2023-06-23 15:48:17 -0300

    adicionei um plugin de copia de arquivos do webpack

--- webpack.config.js
+++ webpack.config.js
@@ -1,16 +1,22 @@
 const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
+const CopyWebpackPlugin = require('copy-webpack-plugin');
 
 module.exports = {
-    entry: './app/src/js/app.js',
-    output: {
-        filename: 'bundle.js',
-        path: path.resolve(__dirname, 'app/dist'),
-        clean: true
-    },
-    plugins: [new HtmlWebpackPlugin({
-        template: './app/src/app.html',
-        filename: 'app.html',
-        hash: true
-    })]
+  entry: './app/src/js/app.js',
+  output: {
+    filename: 'bundle.js',
+    path: path.resolve(__dirname, 'app/dist'),
+    clean: true,
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: './app/src/app.html',
+      filename: 'app.html',
+      hash: true,
+    }),
+    new CopyWebpackPlugin({
+      patterns: [{ from: './app/src/css', to: 'css' }],
+    }),
+  ],
 };
